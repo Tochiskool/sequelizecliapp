4a74c11d11f371a733dd66b9251b1651caa795c7
Tochiskool <mrchristian79@yahoo.com>
2024-04-05 02:13:55 +0200

    changes have been made to the config file and messages have been writen to remember why certain things act they way the do or how they will actually act and what you should do.

--- models/user.js
+++ models/user.js
@@ -5,3 +5,7 @@ modules.exports = function(sequelize,DataTypes){
     });
     return User;
 }
+
+//make sure your database is created and run it actually with the lighting sign
+// when you run the app a table with this colums will be created in the sequelize db that you created
+// An Id will be set automatically with other data.
